a2793293beb2123e893cefc7f655382a09ef0c9e
alineraquel2001-collab <alineraquel2001@gmail.com>
2026-04-29 19:36:52 -0300

    Captura de pacotes

---
+++ 29.04.2026/captura.py
@@ -0,0 +1,9 @@
+#Fazer captura no wireshark salvar em tcpdump
+'''REspoder:
+- Quantos pacotes foram capturados
+- Quais endereços MAC presentes
+- Quantos pacotes IPV4 foram salvos
+- Considerando só IPV4 quantos pacotes TCP e UDP
+foram capturados
+- Quais as duas máquinas mais se 'falaram'? Quantos
+pacotes (considerar só IPV4) '''
